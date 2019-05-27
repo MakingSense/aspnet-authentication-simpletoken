9ff58f9b281bae54433b7811af24fd7b2dd6f7f8
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2019-05-27 17:57:31 -0300

    Create SimpleTokenAuthenticationExtensions.cs
    This allow configure the SimpleTokenAuthentication by extension methods for AuthenticationBuilder

---
+++ src/MakingSense.AspNetCore.Authentication.SimpleToken/SimpleTokenAuthenticationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+
+namespace MakingSense.AspNetCore.Authentication.SimpleToken
+{
+	public static class SimpleTokenAuthenticationExtensions
+	{
+		public static AuthenticationBuilder AddSimpleTokenAuthentication(this AuthenticationBuilder builder)
+			=> builder.AddSimpleTokenAuthentication(SimpleTokenDefaults.AuthenticationScheme, _ => { });
+
+		public static AuthenticationBuilder AddSimpleTokenAuthentication(this AuthenticationBuilder builder,
+			Action<SimpleTokenAuthenticationOptions> configureOptions)
+			=> builder.AddSimpleTokenAuthentication(SimpleTokenDefaults.AuthenticationScheme, configureOptions);
+
+		public static AuthenticationBuilder AddSimpleTokenAuthentication(this AuthenticationBuilder builder,
+			string authenticationScheme,
+			Action<SimpleTokenAuthenticationOptions> configureOptions)
+			=> builder.AddSimpleTokenAuthentication(authenticationScheme, SimpleTokenDefaults.DisplayName, configureOptions: configureOptions);
+
+		public static AuthenticationBuilder AddSimpleTokenAuthentication(this AuthenticationBuilder builder,
+			string authenticationScheme,
+			string displayName,
+			Action<SimpleTokenAuthenticationOptions> configureOptions)
+		{
+			return builder.AddScheme<SimpleTokenAuthenticationOptions, SimpleTokenAuthenticationHandler>(authenticationScheme, displayName, configureOptions);
+		}
+	}
+}

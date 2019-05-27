1e7c20c0cc553ff0e9be7c6ef14a64c17795b11c
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2019-05-27 17:57:01 -0300

    Create SimpleTokenDefaults.cs

---
+++ src/MakingSense.AspNetCore.Authentication.SimpleToken/SimpleTokenDefaults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakingSense.AspNetCore.Authentication.SimpleToken
+{
+	public static class SimpleTokenDefaults
+	{
+		public const string AuthenticationScheme = "SimpleToken";
+
+		public static readonly string DisplayName = "Simple Token";
+	}
+}
